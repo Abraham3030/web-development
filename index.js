6dbb41f89d35cb1daa8d0eaf2ec38116aaef65f7
Abraham Montelongo <mocapunk@hotmail.com>
2021-06-20 01:57:53 -0500

    primer commit entrenamiento accenture

---
+++ index.js
@@ -0,0 +1,16 @@
+const express   = require('express');
+const path      = require('path');
+
+const app   = express();
+const PORT  = process.env.PORT || 3000;
+const publicPath = path.resolve(__dirname, './public');
+
+app.use( express.static( publicPath ) );
+
+app.get( '/', (req, res) => {
+    res.sendFile( path.resolve( __dirname, './views/index.html'));
+});
+
+app.listen( PORT, () => {
+    console.log('Servidor corriendo en el puerto: ', PORT);
+});
